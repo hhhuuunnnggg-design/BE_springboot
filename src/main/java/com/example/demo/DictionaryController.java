6f645f1d3714b66bd206bef7b80aebccf53b0030
hhhuuunnnggg-design <nguyendinhhungtc2020@gmail.com>
2024-11-14 11:26:54 +0700

    .

--- src/main/java/com/example/demo/DictionaryController.java
+++ src/main/java/com/example/demo/DictionaryController.java
@@ -17,36 +17,11 @@ public class DictionaryController {
 
     @GetMapping("/dictionary")
     public ResponseEntity<String> getTranslation(@RequestParam(defaultValue = "") String word) {
-        // Lấy nghĩa từ từ điển và xử lý chữ hoa thành chữ thường
         String translate = dictionaryMap.get(word.trim().toLowerCase());
-        // Nếu có nghĩa của từ
         if (translate != null) {
             return ResponseEntity.ok(translate);
         }
-        // Trả về lỗi nếu không tìm thấy từ
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Không tìm thấy dữ liệu trong từ điển");
-    }
-
-    // bài 3
-    @GetMapping("/calculator")
-    public ResponseEntity<String> getcalculator(@RequestParam(defaultValue = "") String number1,
-            @RequestParam(defaultValue = "") String number2,
-            @RequestParam(defaultValue = "") String thuchien) {
-
-        if (number1.isEmpty()) {
-            return ResponseEntity.badRequest().body("number 1 null");
-        } else if (number2.isEmpty()) {
-            return ResponseEntity.badRequest().body("number 2 null");
-        } else if (thuchien.isEmpty()) {
-            return ResponseEntity.badRequest().body("thuc hien null");
-        }
-
-        // chuyen string thanh number
-        double firstnumber1 = Double.parseDouble(number1);
-        double firstnumber2 = Double.parseDouble(number2);
-        double result;
-
-        return null;
     }
 
 }
